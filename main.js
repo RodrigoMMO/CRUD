a01d12806a394d5adf271b66f0fdba8021f0e127
RodrigoMMO <60015971+RodrigoMMO@users.noreply.github.com>
2020-03-27 18:01:16 -0500

    CRUD
    Intento de CRUD

---
+++ main.js
@@ -0,0 +1,62 @@
+class Contacto {
+    constructor(nombre){
+        this.nombre= nombre;
+    }
+
+}
+
+
+class UI{
+    AgregarContacto(contacto){
+        const lista =    document.getElementById('lista');
+        const elemento=document.createElement('div');
+        elemento.innerHTML=`
+            <div>
+                <strong>Contacto</strong>: ${contacto.nombre}  <button name="Eliminar">Eliminar</button>
+                <button name="Actualizar">Actualizar</button>
+        `;
+        lista.appendChild(elemento);
+        this.reiniciarUI();
+    }
+    reiniciarUI(){
+            document.getElementById('Agendado').reset();
+
+    }
+        EliminarContacto(elemento){
+            if(elemento.name === 'Eliminar'){
+                elemento.parentElement.remove()
+            }
+           
+
+    }
+
+    ActualizarContacto(elemento)
+    {
+        if(elemento.name === 'Actualizar')
+        {
+           
+        }
+    }
+}
+
+//  Eventos
+document.getElementById('Agendado')
+            .addEventListener('submit',function(e){ 
+             const contacto = new Contacto(document.getElementById('Nombre').value)
+             console.log(contacto);
+
+             const ui = new UI()
+
+             ui.AgregarContacto(contacto);
+            
+
+             e.preventDefault();
+            }
+            )
+document.getElementById('lista')
+            .addEventListener('click',function(e){
+                 ui =new UI();
+                 ui.EliminarContacto(e.target);
+                 ui.ActualizarContacto(e.target)
+                ;
+            })
